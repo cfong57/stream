ea1e852f9506e9181e1fe3daa20bc1d67e6dd5ee
Christopher Fong <christopher.m.fong@gmail.com>
2013-10-29 17:18:04 -0700

    devise should redirect to previous page after sign-in/update/sign-out

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  after_filter :store_location
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to root_url, :alert => exception.message
@@ -8,4 +9,31 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     root_url
   end  
+
+  def store_location
+    # store last url - this is needed for post-login redirect to whatever the user last visited.
+    # don't store ajax calls
+    if(request.fullpath != "/users/sign_in" &&
+        request.fullpath != "/users/sign_up" &&
+        request.fullpath != "/users/password" &&
+        !request.xhr? &&
+        request.format == "text/html" || 
+        request.content_type == "text/html") 
+      session[:previous_url] = request.fullpath 
+      session[:last_request_time] = Time.now.utc.to_i
+    end
+  end
+
+  def after_sign_in_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+  def after_update_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
+  def after_sign_out_path_for(resource)
+    session[:previous_url] || root_path
+  end
+
 end
